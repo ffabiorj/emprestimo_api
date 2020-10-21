58283615eca6b30a549da6f3c5824992456315de
Fabio Oliveira <fabio20rj@gmail.com>
2020-10-21 08:26:43 -0300

    Refatorado test criate model.

--- core/tests/test_model.py
+++ core/tests/test_model.py
@@ -1,48 +1,48 @@
-import pytest
 from core.models import Emprestimo, Pagamento
 from django.contrib.auth.models import User
+from django.test import TestCase
 
 
-@pytest.fixture
-def cria_user():
-    return User.objects.create_user(username="fabio", password="12345678")
+class EmprestimoModelTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username="teste", password="teste")
+        self.emprestimo = Emprestimo.objects.create(
+            valor_nominal=10000,
+            taxa_juros=10,
+            endereco_ip="127.0.0.1",
+            data_solicitacao="2020-10-10",
+            banco="Santander",
+            cliente="Petro",
+            owner=self.user,
+        )
+
+    def teste_conta_um_emprestimo(self):
+        assert Emprestimo.objects.count() == 1
+
+    def test_verifica_str(self):
+        assert self.emprestimo.__str__() == "Cliente: teste valor: R$ 10000"
 
 
-@pytest.fixture
-def cria_emprestimo(cria_user):
-    return Emprestimo.objects.create(
-        valor_nominal=10000,
-        taxa_juros=10,
-        endereco_ip="127.0.0.1",
-        data_solicitacao="2020-10-10",
-        banco="Santander",
-        cliente="Petro",
-        owner=cria_user,
-    )
+class PagamentoModelTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username="teste", password="teste")
+        self.emprestimo = Emprestimo.objects.create(
+            valor_nominal=10000,
+            taxa_juros=10,
+            endereco_ip="127.0.0.1",
+            data_solicitacao="2020-10-10",
+            banco="Santander",
+            cliente="Petro",
+            owner=self.user,
+        )
+        self.pagamento = Pagamento.objects.create(
+            data_pagamento="2020-10-10",
+            valor_pagamento=500.00,
+            emprestimo=self.emprestimo,
+        )
 
+    def test_conta_um_pagamento(self):
+        assert Pagamento.objects.count() == 1
 
-@pytest.fixture
-def cria_pagamento(cria_emprestimo):
-    return Pagamento.objects.create(
-        data_pagamento="2020-10-10", valor_pagamento=500.00, emprestimo=cria_emprestimo
-    )
-
-
-@pytest.mark.django_db
-def test_cria_emprestimo(cria_emprestimo):
-    assert Emprestimo.objects.count() == 1
-
-
-@pytest.mark.django_db
-def test_str_emprestimo(cria_emprestimo):
-    assert cria_emprestimo.__str__() == "Cliente: fabio valor: R$ 10000"
-
-
-@pytest.mark.django_db
-def test_cria_pagamento(cria_pagamento):
-    assert Pagamento.objects.count() == 1
-
-
-@pytest.mark.django_db
-def test_str_pagamento(cria_pagamento):
-    assert cria_pagamento.__str__() == "2020-10-10"
+    def test_verifica_str(self):
+        assert self.pagamento.__str__() == "2020-10-10"
